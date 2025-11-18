4f23866e733f743ea42d9d326e3d7d76786d2c6f
Gam!n3 <47696354+OliverSolomon@users.noreply.github.com>
2025-11-18 07:14:49 +0000

    fix:remove unneccesary padding

--- app/eulogy/page.tsx
+++ app/eulogy/page.tsx
@@ -5,7 +5,7 @@ export default function Page({ searchParams }: { searchParams?: { url?: string }
 
   return (
     <main className="py-12">
-      <div className="lg:max-w-5xl mx-auto px-6">
+      <div className=" mx-auto lg:px-6">
         <EulogyViewer initialUrl={url} />
       </div>
     </main>
